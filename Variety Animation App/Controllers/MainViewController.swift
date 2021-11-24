8fb319681ba994947af5b042f6b34b62ec739837
Alik <Nigay_AO@bk.ru>
2021-11-24 22:10:39 +0300

    Make some improves

--- Variety Animation App/Controllers/MainViewController.swift
+++ Variety Animation App/Controllers/MainViewController.swift
@@ -19,36 +19,26 @@ class MainViewController: UIViewController {
     
     @IBOutlet weak var buttonLabel: SpringButton!
     
-    private let animation = AnimationModel.getAnimation()
+    private let animations = AnimationModel.getAnimation()
+    private var count = 0
     private var isFirtsStart = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        initialSetup(animation)
+                initialSetup(animations[count])
     }
     
     @IBAction func springButtonPressed(_ sender: SpringButton) {
-        isFirtsStart = false
-        let newAnimation = AnimationModel.getAnimation()
-        buttonLabel.setTitle("Run \(newAnimation.present)", for: .normal)
-        
-        if isFirtsStart {
-            springView.animation = animation.present
-            springView.curve = animation.curve
-            springView.force = animation.force
-            springView.duration = animation.duration
-            springView.delay = animation.delay
-            springView.animate()
-        } else {
-            initialSetup(newAnimation)
-            springView.animation = newAnimation.present
-            springView.curve = newAnimation.curve
-            springView.force = newAnimation.force
-            springView.duration = newAnimation.duration
-            springView.delay = newAnimation.delay
-            springView.animate()
-        }
-
+        let animation = animations[count]
+        buttonLabel.setTitle("Run \(animations[updateCount()].present)", for: .normal)
+        springView.animation = animation.present
+        springView.curve = animation.curve
+        springView.force = animation.force
+        springView.duration = animation.duration
+        springView.delay = animation.delay
+        springView.animate()
+        updateCount()
+        initialSetup(animation)
     }
     
     private func initialSetup(_ animation: AnimationModel) {
@@ -57,6 +47,15 @@ class MainViewController: UIViewController {
         forceLabel.text = "Force: \(String(format: "%.2f", animation.force))"
         durationLabel.text = "Duration: \(String(format: "%.2f", animation.duration))"
         delayLabel.text = "Delay: \(String(format: "%.2f", animation.delay))"
+    }
+    
+    private func updateCount() -> Int {
+        if count < animations.count {
+            count += 1
+        } else {
+            count = 0
+        }
+        return count
     }
     
 }
--- Variety Animation App/Models/AnimationModel.swift
+++ Variety Animation App/Models/AnimationModel.swift
@@ -14,17 +14,25 @@ struct AnimationModel {
     let duration: CGFloat
     let delay: CGFloat
     
-    static func getAnimation() -> AnimationModel {
+    static func getAnimation() -> [AnimationModel] {
+        var allInOne:[AnimationModel] = []
+        
         let animations = Spring.AnimationPreset.allCases
         let curves = Spring.AnimationCurve.allCases
         let force = CGFloat.random(in: 1...2)
         let duration = CGFloat.random(in: 1...2)
         let delay = CGFloat.random(in: 0.1...0.3)
-        let animaion = AnimationModel(present: animations.randomElement()?.rawValue ?? "",
-                                      curve: curves.randomElement()?.rawValue ?? "",
-                                      force: force,
-                                      duration: duration,
-                                      delay: delay)
-        return animaion
+        
+        let minimum = min(animations.count, curves.count)
+        
+        for index in 0..<minimum {
+            let animaion = AnimationModel(present: animations.shuffled()[index].rawValue,
+                                          curve: curves.shuffled()[index].rawValue,
+                                          force: force,
+                                          duration: duration,
+                                          delay: delay)
+            allInOne.append(animaion)
+        }
+        return allInOne
     }
 }
